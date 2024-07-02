32009546a4b33a11be68f3869d1a5f84b529735a
EldsonC <eldson.caldasw@gmail.com>
2024-07-02 19:26:59 -0300

    🚀 new-server

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ app.use(cors());
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://powtfy.vercel.app/',
+    origin: '*',
     methods: ['GET', 'POST']
   }
 });
